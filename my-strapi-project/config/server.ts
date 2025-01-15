14f8f26f1ae1f9af5b44f1cb7ce5f948bff7dcb6
TellecheaEsteban <tellecheaestebanm@gmail.com>
2025-01-15 17:07:18 -0300

    modified server.ts due to a problem with render not finding the port

--- my-strapi-project/config/server.ts
+++ my-strapi-project/config/server.ts
@@ -1,12 +1,16 @@
-export default ({ env }) => {
-  const port = parseInt(process.env.PORT, 10) || 1337;
+export default () => {
+  const port = parseInt(process.env.PORT, 10);
+  if (!port) {
+    throw new Error("Environment variable PORT is not defined!");
+  }
+
   console.log(`Starting Strapi on host: 0.0.0.0, port: ${port}`);
 
   return {
     host: "0.0.0.0",
     port,
     app: {
-      keys: env.array("APP_KEYS"),
+      keys: process.env.APP_KEYS ? process.env.APP_KEYS.split(",") : [],
     },
   };
 };
